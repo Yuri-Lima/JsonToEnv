9830db2ae522236ab13ada0e0e06e72deb5cc647
Yuri-Lima <y.m.lima19@gmail.com>
2022-08-20 14:41:18 +0200

    📖 DOC:  Comments some lines👌 IMPROVE: Removed test Class

--- src/index.ts
+++ src/index.ts
@@ -244,13 +244,18 @@ export class JsonToEnv {
     /**
      * @description: Write the new .env file
      */
+    /**
+     * if createNewEnvFile is true and test is false, then create a new .env file overwriting the old one
+     * if createNewEnvFile is true and test is true, then create a new .env file withouth overwriting the old one
+     * if createNewEnvFile is false, then do not create a new .env file
+     */
     if (this.options.createNewEnvFile) {
-      if (this.options.test) {
+      if (this.options.test) {// If the test is true, then it will not over write the original .env file
         fs.writeFileSync(
           `.env.${this.getRandomInt(1, 100000)}`,
           env.join('\n'),
         );
-      } else {
+      } else { // If the test is false, then it will over write the original .env file
         fs.writeFileSync(
           `${resolve(process.cwd(), '.env')}`,
           env.join('\n'),
@@ -259,32 +264,3 @@ export class JsonToEnv {
     }
   }
 }
-
-/**
- * ATTENTION: If you dont want to overwrite your original .env file, set this to true
- * @description: This function will read the env.json file and set the env variables
- * @param {string} filePath - The path of the env.json file
- * @param {boolean} test - ATTENTION: If you dont want to overwrite your original .env file, set this to true
- * @param {boolean} createNewJsonFile - If you want to create a new json file, set this to true
- * @param {boolean} useCache - If you want to use the cache to compare the previous json file and the new json file, set this to true
- * @param {boolean} useNull - If you want to set the value to null, set this to true
- * @param {boolean} useUndefined - If you want to set the value to undefined, set this to true
- * @param {string} jsonFile - If you want to directly set the json file
- * @param {string} json - If you want to directly set the json
- */
-
-const fileName = 'env.json';
-const set = {
-  fileName: fileName,
-  filePath: join(process.cwd(), 'src', fileName),
-};
-const options ={
-    test: false, // if you dont want to overwrite your original .env file, set this to true
-    updateNewJsonFile: false, // If you want to create a new json file, set this to true
-    createNewEnvFile: true, // If you want to create a new env file, set this to true
-    useCache: false // If you want to use the cache to compare the previous json file and the new json file
-  }
-console.log(`${set.filePath}`);
-const setEnv = new JsonToEnv(set, options);
-setEnv.setEnv();
-console.log(process.env);
