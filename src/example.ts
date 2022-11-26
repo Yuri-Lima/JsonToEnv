1652bf20f20e1a624a6e231b35b285aa82a2bd24
Yuri-Lima <y.m.lima@hotmail.com>
2022-11-26 13:17:28 +0000

    refactor: totally new process
    ESM and CJS support / Fix Types / Semantic release
    
    BREAKING CHANGE: Production Mode

--- src/example.ts
+++ src/example.ts
@@ -1,5 +1,5 @@
-import { join } from "node:path";
-import { JsonToEnv, Set_Env, Options_Set_Env } from "./index";
+import path from "node:path";
+import JsonToEnv from "./index";
 
 /**
  * ATTENTION: If you dont want to overwrite your original .env file, set this to true
@@ -10,20 +10,13 @@ import { JsonToEnv, Set_Env, Options_Set_Env } from "./index";
  * @param {boolean} createEnvFile - If you want to create a new env file, set this to true
  */
 
-  const fileName = 'envTree.json';
-  const set:Set_Env = {
-    fileName: fileName,
-    readFileFrom: join(process.cwd()),
-    saveFileTo: join(process.cwd(), "src", fileName),
-  };
- 
-  const options:Options_Set_Env ={
-      overWrite_Original_Env: true, // if you dont want to overwrite your original .env file, set this to true
-      createJsonFile: true, // If you want to create a new json file, set this to true
-      createEnvFile: true, // If you want to create a new env file, set this to true
-      log:false, // If you want to log the result, set this to true
-  }
-  console.log(set);
-  const setEnv = new JsonToEnv(set, options);
+  const setEnv = new JsonToEnv({
+    readFileFrom: path.join(__dirname, "..","envTree.json"),
+    saveFileTo: path.join(__dirname, "..", ".env")
+  }, {
+    overWrite_Original_Env: true, // if you dont want to overwrite your original .env file, set this to true
+    createJsonFile: true, // If you want to create a new json file, set this to true
+    createEnvFile: true, // If you want to create a new env file, set this to true
+    log: true, // If you want to log the result, set this to true
+  });
   setEnv.setEnv();
-  // console.log(process.env);
